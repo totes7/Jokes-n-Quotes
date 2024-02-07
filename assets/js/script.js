83b7388cef9285006fbd4c54fdd6f3bf2d3d8b9a
Antonio Manno <antonio.manno.7@gmail.com>
2024-02-07 19:20:52 +0000

    replace quotes api to avoid CORS

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,14 @@
 // jokeapi script
-$('#btn-jokes').on('click', getJokes);
+$("#btn-jokes").on("click", getJokes);
 
 function getJokes() {
-  fetch('https://v2.jokeapi.dev/joke/Any/3')
-    .then(response => response.json())
-    .then(data => {
-      const responseContent = document.getElementById('response-content');
-      responseContent.innerHTML = ''; // Clear existing content
+  fetch("https://v2.jokeapi.dev/joke/Any/3")
+    .then((response) => response.json())
+    .then((data) => {
+      const responseContent = document.getElementById("response-content");
+      responseContent.innerHTML = ""; // Clear existing content
 
-      if (data.type === 'twopart') {
+      if (data.type === "twopart") {
         responseContent.innerHTML += `
           <div class="response-card">
             <p><strong>Setup:</strong> ${data.setup}</p>
@@ -21,46 +21,32 @@ function getJokes() {
           </div>`;
       }
     })
-    .catch(error => {
-      console.error('Error fetching jokes:', error.message);
+    .catch((error) => {
+      console.error("Error fetching jokes:", error.message);
     });
-
 }
-
 
 // Daily Inspirational Quotes
 
-$('#btn-quotes-2').on('click', getQuotes);
+$("#btn-quotes-2").on("click", getQuotes);
 
 function getQuotes() {
-    const apiUrl = 'https://zenquotes.io/api/today';
-    fetch(`https://cors-anywhere.herokuapp.com/${apiUrl}`, {
-        method: 'GET',
-      })
-    // fetch(apiUrl)
-    .then(response => response.json())
-    .then(data => {
-      const modalBody = document.querySelector('#quotesModal .modal-body');
-      modalBody.innerHTML = ''; // Clear existing content
-
-      // Check if the data contains quotes
-      if (data.length > 0) {
-        // Display each quote in the modal body
-        data.forEach(quote => {
-          modalBody.innerHTML += `
-            <div class="response-card">
-              <p><strong>Quote:</strong> ${quote.q}</p>
-              <p><strong>Author:</strong> ${quote.a}</p>
-            </div>`;
-        });
-      } else {
-        // If no quotes are available
-        modalBody.innerHTML = '<p>No quotes available at the moment.</p>';
-      }
+  const apiUrl = "https://type.fit/api/quotes";
+  fetch(apiUrl)
+    .then(function (response) {
+      return response.json();
     })
-    .catch(error => {
-      console.error('Error fetching quotes:', error.message);
-    });
-  
-}
+    .then(function (data) {
+      console.log(data);
+      let randomNumber = Math.floor(Math.random() * 15);
+      let quote = data[randomNumber]
+      let quoteText = quote.text
+      let authorData = quote.author;
+      let sections = authorData.split(',');
+      let authorFinal = sections[0];
 
+      $(".modal-body").html(
+        `<div class="response-card"><p><strong>Quote:</strong> ${quoteText}</p><p><strong>Author:</strong> ${authorFinal}</p></div>`
+      );
+    });
+}
